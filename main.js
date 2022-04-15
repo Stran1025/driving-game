31a2c421401cace2f3fef1e04fa13b6ee69463f1
Sang Tran <sang.tran1025@gmail.com>
2022-04-15 22:55:50 +0000

    Completed #5

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 var car = {
   facing: 0,
   position: {
-    x: 0,
-    y: 0
-  }
+    top: 0,
+    left: 0
+  },
+  isMoving: false
 };
 
 var $car = document.querySelector('.car');
+var $hitbox = document.querySelector('.hitbox');
 
 // var $bound = document.querySelector('.bound');
 
 document.addEventListener('keydown', handleKeyPress);
 
 function handleKeyPress(event) {
-  if (event.key !== 'ArrowRight' && event.key !== 'ArrowLeft' && event.key !== 'ArrowUp' && event.key !== 'ArrowDown') {
+  if (event.key !== 'ArrowRight' && event.key !== 'ArrowLeft' && event.key !== 'ArrowUp' && event.key !== 'ArrowDown' && event.key !== ' ') {
     return;
   }
   if (event.key === 'ArrowRight') {
@@ -22,9 +24,33 @@ function handleKeyPress(event) {
     makeTurn(2);
   } else if (event.key === 'ArrowUp') {
     makeTurn(3);
-  } else {
+  } else if (event.key === 'ArrowDown') {
     makeTurn(1);
+  } else if (event.key === ' ') {
+    toggleMoving();
   }
+}
+
+function toggleMoving() {
+  if (car.isMoving) {
+    return;
+  }
+  car.isMoving = true;
+  setInterval(moveCar, 12);
+}
+
+function moveCar() {
+  if (car.facing === 0) {
+    car.position.left += 5;
+  } else if (car.facing === 1) {
+    car.position.top += 5;
+  } else if (car.facing === 2) {
+    car.position.left -= 5;
+  } else if (car.facing === 3) {
+    car.position.top -= 5;
+  }
+  $hitbox.style.left = ' ' + (car.position.left) + 'px';
+  $hitbox.style.top = ' ' + car.position.top + 'px';
 }
 
 function makeTurn(num) {
